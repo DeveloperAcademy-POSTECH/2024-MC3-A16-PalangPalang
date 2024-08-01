738f003da5ee6e9371ded6cf5860515a65d13fdb
bono <shippingbok@gmail.com>
2024-08-02 04:55:10 +0900

    [✨Feat] 설정, 알람 진행, 미션 ViewModel

---
+++ PalangPalang/Sources/Presenters/ViewModel/MissionViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  MissionViewModel.swift
+//  PalangPalang
+//
+//  Created by 박혜운 on 8/1/24.
+//  Copyright © 2024 com.mc3. All rights reserved.
+//
+
+import Foundation
+
+@Observable
+class MissionViewModel {
+  struct State {
+    
+  }
+  
+  enum Action {
+    
+  }
+  
+  private(set) var state: State = .init()
+}
