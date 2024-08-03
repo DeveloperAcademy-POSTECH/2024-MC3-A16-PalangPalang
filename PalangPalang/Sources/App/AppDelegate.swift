31e5eab1a018d4aaf574e33ed782a1459e064ae6
bono <shippingbok@gmail.com>
2024-08-04 01:56:26 +0900

    [🔨Refactor] UseCase 주입 형태로 변경

--- PalangPalang/Sources/App/AppDelegate.swift
+++ PalangPalang/Sources/App/AppDelegate.swift
@@ -4,13 +4,16 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
+  let alarmUseCase = AlarmUseCase()
   
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions:
     [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    let appView = AppView()
+    
+    
+    let appView = AppView(alarmUseCase: alarmUseCase)
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.rootViewController = UIHostingController(rootView: appView)
     window?.makeKeyAndVisible()
