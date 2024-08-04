61da974699df99a85c4524d2437930dbddd3fb6e
Dorii0513 <bobodorii0513@gmail.com>
2024-08-04 21:40:18 +0900

    [💄View]AlarmMainView 디자인 적용

---
+++ PalangPalang/Sources/DesignSystem/Extensions/MainButtonStyle.swift
@@ -0,0 +1,25 @@
+//
+//  MainButtonStyle.swift
+//  PalangPalang
+//
+//  Created by 김예림 on 8/4/24.
+//  Copyright © 2024 com.mc3. All rights reserved.
+//
+
+import SwiftUI
+
+//struct MainButtonStyle: ButtonStyle {
+//    
+//    var textColor: Color
+//    var bgColor: Color
+//    
+//    func makeBody(configuration: Configuration, textColor: Color, bgColor: Color) -> some View {
+//        configuration.label
+//            .palangFont(.textBody02)
+//            .foregroundColor(textColor)
+//            .frame(maxWidth: .infinity, maxHeight: 60)
+//            .background(bgColor)
+//            .cornerRadius(8)
+//    }
+//}
+
--- PalangPalang/Sources/Presenters/Scenes/MainAlarmSettings/AlarmMain.swift
+++ PalangPalang/Sources/Presenters/Scenes/MainAlarmSettings/AlarmMain.swift
@@ -12,29 +12,48 @@ struct AlarmMain: View {
   let alarmViewModel: AlarmSettingsViewModel
 
   var body: some View {
-    Spacer()
-    Clock(alarm: alarmViewModel.state.alarm)
-    
-    Button(
-      action: {
-        alarmViewModel.effect(action: .tappedSettingsButton)
-      },
-      label: {
-        Text("설정하기")
+      VStack {
+          Spacer()
+          
+          Text("AM")
+              .foregroundStyle(.palangGray)
+              .palangFont(.textBody01Bold)
+          
+          Clock(alarm: alarmViewModel.state.alarm)
+              .padding(.bottom,10)
+          
+          Button(
+            action: {
+                alarmViewModel.effect(action: .tappedSettingsButton)
+            },
+            label: {
+                //Text("묙표 시간 설정하기")
+                SettingButton()
+            }
+          )
+          
+          Spacer()
+          
+          Button(
+            action: {
+                alarmViewModel.effect(action: .tappedStartAlarm)
+            },
+            label: {
+                Text("시작하기")
+            }
+          )
+          .disabled(!alarmViewModel.state.readyForStart)
+          .palangFont(.textBody02)
+          .foregroundColor(.palangText03)
+          .frame(maxWidth: .infinity, maxHeight: 60)
+          .background(.palangButton02)
+          .cornerRadius(16)
+          .padding(.horizontal,45)
+          .padding(.bottom,55)
       }
-    )
-    
-    Spacer()
-    
-    Button(
-      action: {
-        alarmViewModel.effect(action: .tappedStartAlarm)
-      },
-      label: {
-        Text("시작하기")
-      }
-    )
-    .disabled(!alarmViewModel.state.readyForStart)
+      .frame(maxWidth: .infinity)
+      .background(.palangYellow)
+      .ignoresSafeArea()
   }
 }
 
@@ -42,14 +61,35 @@ struct AlarmMain: View {
   AlarmMain(alarmViewModel: .init(useCase: AlarmUseCase.init()))
 }
 
+//이렇게 뷰를 만들어 주는 게 맞는지,,?
+private struct SettingButton: View{
+    
+    var body: some View{
+        HStack(spacing: 0) {
+            Text("목표시간 설정하기")
+                .palangFont(.textCaption01)
+            Image(systemName: "chevron.right")
+                .padding(.horizontal,2)
+                .fontWeight(.medium)
+        }
+        .foregroundColor(.palangText00)
+        
+    }
+}
+
 private struct Clock: View {
   let alarm: AlarmSettingsModel
   
   var body: some View {
-    HStack {
+    HStack(spacing: 0) {
       Text("\(alarm.hour)")
+            .palangFont(.numH2)
+            .padding(.horizontal,7)
       Text(":")
+            .palangFont(.numSymbol02)
       Text("\(alarm.minutes)")
-    }
+            .palangFont(.numH2)
+            .padding(.horizontal,7)
+    }.foregroundColor(.palangText00)
   }
 }
