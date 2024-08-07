c8ba56b4bfacfe91fca4e4ce63b75cbbe3119976
bono <shippingbok@gmail.com>
2024-08-08 04:17:04 +0900

    [✏️Comment] 사소한 코드 수정 및 코드 위치 변경

--- PalangPalang/Sources/Presenters/Scenes/MissionOnProcess/MissionTimeoutView.swift
+++ PalangPalang/Sources/Presenters/Scenes/MissionOnProcess/MissionTimeoutView.swift
@@ -9,76 +9,88 @@
 import SwiftUI
 
 struct MissionTimeoutView: View {
-  @State private var moveDistance: CGFloat = 0 // 이동 거리
-  @State private var isTrailing = false
-  @State private var lineLength: CGFloat = 0 // 초기 선 길이
   let useCase: MissionCompleted
   
   var body: some View {
     ZStack(alignment: .top) {
       
-      HStack {
-        PalangPalangAsset.Assets.finalGhostLeading.swiftUIImage
-          .padding(.leading, 34)
-          .padding(.top, 28)
-        
-        Spacer()
-        
-        PalangPalangAsset.Assets.finalGhostTrailing.swiftUIImage
-          .padding(.trailing, 36)
-          .padding(.top, 87)
-      }
+      SmallTwoGhost()
       
-      VStack {
-        
-        Spacer()
-        
-        Text("생각 귀신에게\n먹혔어요")
-          .palangFont(.textH1)
-          .foregroundStyle(.gray)
-          .multilineTextAlignment(.center)
-          .padding(.bottom, 21)
-        
-        Text("다음엔 꼭 생각귀신을 돌파해보세요!")
-          .palangFont(.textCaption02)
-          .foregroundStyle(.gray)
-          .padding(.bottom, 75)
-        
-        Button {
-          useCase.endAlarm()
-        } label: {
-          Text("메인으로")
-        }
-      }
+      InfoLabelAndGoToHomeInfo(useCase: useCase)
+    }
+    .background {
+      BigBackGroundGhost()
     }
     .background(.palangGray)
   }
 }
 
-
-struct TrianglePath: Shape {
-  var moveDistance: CGFloat
-  
-  // 애니메이션 가능한 데이터를 지정
-  var animatableData: CGFloat {
-    get { moveDistance }
-    set { moveDistance = newValue }
+private struct SmallTwoGhost: View {
+  var body: some View {
+    HStack {
+      PalangPalangAsset.Assets.finalGhostLeading.swiftUIImage
+        .padding(.leading, 34)
+        .padding(.top, 28)
+      
+      Spacer()
+      
+      PalangPalangAsset.Assets.finalGhostTrailing.swiftUIImage
+        .padding(.trailing, 36)
+        .padding(.top, 87)
+    }
   }
+}
+
+private struct InfoLabelAndGoToHomeInfo: View {
+  let useCase: MissionCompleted
   
-  func path(in rect: CGRect) -> Path {
-    var path = Path()
-    
-    // A, B, C 좌표 설정
-    let A = CGPoint(x: rect.midX, y: rect.minY)
-    let B = CGPoint(x: rect.midX, y: rect.midY)
-    let C = CGPoint(x: rect.midX + moveDistance, y: rect.midY)
-    
-    path.move(to: A)
-    path.addLine(to: B)
-    path.addLine(to: C)
-    path.addLine(to: A)
-    
-    return path
+  var body: some View {
+    VStack {
+      
+      Spacer()
+      Text("생각 귀신에게\n먹혔어요")
+        .palangFont(.textH1)
+        .foregroundStyle(.palangGray)
+        .multilineTextAlignment(.center)
+        .padding(.bottom, 21)
+      
+      Text("다음엔 꼭 생각귀신을 돌파해보세요!")
+        .palangFont(.textCaption02)
+        .foregroundStyle(.palangGray)
+        .padding(.bottom, 75)
+      
+      Button(
+        action: {
+          useCase.endAlarm()
+        },
+        label: {
+          Text("메인으로")
+            .frame(maxWidth: .infinity)
+            .frame(height: 60)
+        }
+      )
+      .palangFont(.textBody02)
+      .foregroundColor(.palangText00)
+      .background(.palangYellow)
+      .cornerRadius(16)
+      .padding(.horizontal, 44)
+      .padding(.bottom, 34)
+    }
+  }
+}
+
+private struct BigBackGroundGhost: View {
+  var body: some View {
+    VStack {
+      Spacer()
+      
+      PalangPalangAsset.Assets
+        .missionOnPrecessGhost.swiftUIImage
+        .resizable()
+        .scaledToFit()
+        .frame(width: 637)
+        .offset(y: 150)
+    }
   }
 }
 
