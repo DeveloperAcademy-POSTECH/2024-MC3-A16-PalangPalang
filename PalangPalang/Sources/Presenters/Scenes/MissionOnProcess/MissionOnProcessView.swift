760e2788ae67c2f77d2c6fb37072ec9a75098839
bono <shippingbok@gmail.com>
2024-08-08 04:08:34 +0900

    [🔨Refactor] MissionOnProcessView 레이어 분리 및 디리니 추가

--- PalangPalang/Sources/Presenters/Scenes/MissionOnProcess/MissionOnProcessView.swift
+++ PalangPalang/Sources/Presenters/Scenes/MissionOnProcess/MissionOnProcessView.swift
@@ -2,32 +2,9 @@
 //  MissionOnProcessView.swift
 //  PalangPalang
 //
-//  Created by 박혜운 on 8/1/24.
+//  Created by 조민 on 8/1/24.
 //  Copyright © 2024 com.mc3. All rights reserved.
 //
-
-//import SwiftUI
-//
-//struct MissionOnProcessView: View {
-//  let viewModel: MissionOnProcessViewModel
-//
-//  var body: some View {
-//    VStack {
-//      Text("이 시간 안에 도망가시옹 \(viewModel.state.timerM):\(viewModel.state.timerS)")
-//      Text("\(viewModel.state.nowSteps)")
-//    }
-//    .onAppear {
-//      viewModel.effect(action: ._onAppear)
-//    }
-//    .onDisappear {
-//      viewModel.effect(action: ._onDisappear)
-//    }
-//  }
-//}
-//
-//#Preview {
-//  MissionOnProcessView(viewModel: .init(useCase: AlarmUseCase.init()))
-//}
 
 import SwiftUI
 
@@ -43,7 +20,8 @@ struct MissionOnProcessView: View {
         .multilineTextAlignment(.center)
         .padding(.top, 47)
       
-      Image("MissionOnPrecessGhost")
+      PalangPalangAsset.Assets
+        .missionOnPrecessGhost.swiftUIImage
         .resizable()
         .frame(width: 266, height: 310)
         .padding(.top, 55)
@@ -80,30 +58,28 @@ struct MissionOnProcessView: View {
 }
 
 struct RemainingStepText: View {
-  
   let viewModel: MissionOnProcessViewModel
   
   var body: some View {
-    HStack(spacing: 0){
-      ZStack{
-        Image("RemainingStepCircle")
-          .resizable()
-          .frame(maxWidth: .infinity, maxHeight: 196)
+    ZStack {
+      PalangPalangAsset.Assets
+        .remainingStepCircle.swiftUIImage
+        .resizable()
+        .frame(maxWidth: .infinity, maxHeight: 196)
+      
+      HStack(spacing: 9) {
+        Text("\(viewModel.state.nowSteps)")
+          .palangFont(.numH4)
+          .padding(.horizontal, 17)
+          .padding(.vertical, 7.5)
+          .background(
+            RoundedRectangle(cornerRadius: 16)
+              .fill(.palangButton02)
+          )
         
-        HStack(spacing: 9){
-          Rectangle()
-            .fill(.palangButton02)
-            .clipShape(RoundedRectangle(cornerRadius: 16))
-            .frame(maxWidth: 90, maxHeight: 72)
-            .overlay {
-              Text("\(viewModel.state.nowSteps)")
-                .palangFont(.numH4)
-            }
-          
-          Text("걸음")
-            .palangFont(.textH2)
-            .foregroundStyle(.palangText00)
-        }
+        Text("걸음")
+          .palangFont(.textH2)
+          .foregroundStyle(.palangText00)
       }
     }
   }
